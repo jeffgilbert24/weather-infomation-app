e42fcff7377d6ca52a7ae6431235283a06d6a742
IBBear <jgilbert868@gmail.com>
2021-07-05 08:15:30 -0700

    created js file

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var citySearchEl = document.querySelector(".search-input");
+var APIKey = "b2972dde250b34760d86f87c6e7c4c49";
+
+function SearchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.querySelector('.search-input').value;
+}
