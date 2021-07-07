529d8c902a2a20a922aa107da5566aa909d4871d
IBBear <jgilbert868@gmail.com>
2021-07-06 18:45:00 -0700

    Changes js file to displat info in html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
-var citySearchEl = document.querySelector(".search-input");
+var citySearchEl = document.querySelector(".textBox");
+var searchButton = document.querySelector("#searchBtn")
+var resultContentEl = document.querySelector("#result-content");
+var resultTextEl = document.querySelector("#result-text");
+
 var APIKey = "b2972dde250b34760d86f87c6e7c4c49";
 
-function SearchFormSubmit(event) {
-    event.preventDefault();
+//var formSubmitHandler = function (event){
+  //  event.preventDefault();
+    //var cityName = citySearchEl.nodeValue.trim();
 
-    var searchInputVal = document.querySelector('.search-input').value;
+    //if(cityName){}
+//}
+
+
+var weather =  {
+    getWeather: function(citySearchEl){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+citySearchEl+"&appid=" +APIKey)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                displayWeather(data);;
+            });
+        }else{
+            alert('Error: ' + response.statusText);
+        }
+    });
+    },    
+};
+var displayWeather = function(data){
+    const {name} = data;
+    const {temp,humidity} = data.main;
+    const {speed} = data.wind;
+    console.log(name, humidity, speed);
+    document.querySelector("#city-name").innerHTML = name;
+
 }
+
+
+    
+        
